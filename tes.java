1682d5ba734f2e54adf1f38dc1e0859a642b4978
Rahmatullah. R <50456560+fnilvuwu@users.noreply.github.com>
2023-04-11 15:18:56 +0800

    Add files via upload

---
+++ tes.java
@@ -0,0 +1,11 @@
+/**
+ * tes
+ */
+public class tes {
+
+    public static void main(String[] args) {
+        String iniString = "TES-TIS";
+        String[] arrayString = iniString.split("\\-");
+        System.out.println(arrayString);
+    }
+}
