b1a0d62fc7a659633bab02de60399274d0d967c3
NaaoiCode <muhnabil7604@gmail.com>
2023-04-12 15:04:04 +0800

    Tugas Asistensi Pertemuan-4

---
+++ H071221083/Pertemuan-4/Nomor2/SelfUtils.java
@@ -0,0 +1,8 @@
+package Tugas4Nomor2;
+
+public class SelfUtils {
+    public static void selfDisplay() {
+        System.out.println("Muhammad Nabil Shadiq");
+        System.out.println("H071221083");
+    }   
+}
