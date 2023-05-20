6431d6d6a1b2f05331e7a8b4262c8d56efc2380f
fnilvuwu <apple@apples-MacBook-Pro.local>
2023-05-20 12:11:42 +0800

    Praktikum 8

---
+++ H071221001/Tugas Praktikum 8/Soal 2/Typer.java
@@ -0,0 +1,57 @@
+
+class Typer extends Thread {
+    private String botName, wordsTyped;
+    private double wpm;
+    private TypeRacer typeRacer;
+
+    public Typer(String botName, double wpm, TypeRacer typeRacer) {
+        this.botName = botName;
+        this.wpm = wpm;
+        this.wordsTyped = "";
+        this.typeRacer = typeRacer;
+
+    }
+
+    public String getBotName() {
+        return botName;
+    }
+
+    public void setBotName(String botName) {
+        this.botName = botName;
+    }
+
+    public String getWordsTyped() {
+        return wordsTyped;
+    }
+
+    public void addWordTyped(String newWordsTyped) {
+        this.wordsTyped += newWordsTyped + " ";
+    }
+
+    public double getWpm() {
+        return wpm;
+    }
+
+    public void setWpm(double wpm) {
+        this.wpm = wpm;
+    }
+
+    @Override
+    public void run() {
+        String[] wordsToType = typeRacer.getWordsToType().split(" ");
+        int howLongToType = 1000;
+        for (String word : wordsToType) {
+            try {
+                Thread.sleep(howLongToType);
+                addWordTyped(word);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        this.addWordTyped("(selesai)");
+
+        typeRacer.addResult(this);
+    }
+
+}
